f855ab40d16680270d5d030a163a0e6a8cc73885
JingjingRenabc <74656977+JingjingRenabc@users.noreply.github.com>
2022-06-29 21:41:39 +0800

    Update generate_txt.py

--- data/generate_txt.py
+++ data/generate_txt.py
@@ -1,18 +1,5 @@
 import os
-#img_dir = '/home/jingjingren/abc_project/taming-transformers-master/data/mydata/xingxing_t'
-#img_dir = '/mnt/wfs/mmchongqingwfssz/project_mm-base-vision/jingjingren/taming-transformers-master/data/mydata/xingxing_t'
-#img_dir = '/home/ubuntu/Workspace/wy/dataset/DAVIS/JPEGImages/480p/hike/'
-# img_dir = '/home/ubuntu/Workspace/abc/dataset/RESIDE-6K/test/hazy/'
-# #img_dir = '/mnt/wfs/mmchongqingwfssz/project_mm-base-vision/data/youtube-vos/test/generated_masks/02b1a46f42'
-# imgs = sorted(os.listdir(img_dir))
-# f = open("/home/ubuntu/Workspace/abc/dlformer_demo_pkg/dlformer_demo/data/txt_files/haze_test_hazzzz.txt",'a')
-# for i in imgs:
-#     f.write(os.path.join(img_dir, i) +'\n')
-#print(i)
-
-
-
-#cod_video_dir = '/home/ubuntu/Workspace/abc/dataset/DAVIS/test/JPEGImages/camel/'
+# to generate txt file list of the frames and masks of the video
 frame_dir = 'data/JPEGImages/soccerball'
 mask_dir = 'data/Annotations/soccerball'
 f = open("data/txt_files/soccerball.txt",'a')
